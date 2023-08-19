bfc9a231d78639eb8199ac611a7bb508b51e92c6
Glen Guarin <glen.guarin@gmail.com>
2023-08-20 06:13:53 +1200

    updated carousel

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,135 +1,30 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+import React, { useState, useEffect } from "react";
+import Carousel from "react-bootstrap/Carousel";
 
-const rightArrow = ">";
-const leftArrow = "<";
-const slideStyles = {
-  width: "100%",
-  height: "100%",
-  borderRadius: "10px",
-  backgroundSize: "cover",
-  backgroundPosition: "center",
-};
-
-const rightArrowStyles = {
-  position: "absolute",
-  top: "50%",
-  transform: "translate(0, -50%)",
-  right: "16px",
-  fontSize: "45px",
-  color: "#fff",
-  zIndex: 1,
-  cursor: "pointer",
-};
-
-const leftArrowStyles = {
-  position: "absolute",
-  top: "50%",
-  transform: "translate(0, -50%)",
-  left: "16px",
-  fontSize: "45px",
-  color: "#fff",
-  zIndex: 1,
-  cursor: "pointer",
-};
-
-const sliderStyles = {
-  position: "relative",
-  height: "100%",
-};
-
-const dotsContainerStyles = {
-  display: "flex",
-  justifyContent: "center",
-};
-
-const dotStyle = {
-  margin: "0 3px",
-  cursor: "pointer",
-  fontSize: "20px",
-};
-
-const slidesContainerStyles = {
-  display: "flex",
-  height: "100%",
-  transition: "transform ease-out 1s",
-};
-
-const slidesContainerOverflowStyles = {
-  overflow: "hidden",
-  height: "100%",
-};
-
-const ImageSlider = ({ slides, parentWidth }) => {
-  const timerRef = useRef(null);
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const goToPrevious = () => {
-    const isFirstSlide = currentIndex === 0;
-    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
-    setCurrentIndex(newIndex);
-  };
-  const goToNext = useCallback(() => {
-    const isLastSlide = currentIndex === slides.length - 1;
-    const newIndex = isLastSlide ? 0 : currentIndex + 1;
-    setCurrentIndex(newIndex);
-  }, [currentIndex, slides]);
-  const goToSlide = (slideIndex) => {
-    setCurrentIndex(slideIndex);
-  };
-  const getSlideStylesWithBackground = (slideIndex) => ({
-    ...slideStyles,
-    backgroundImage: `url(${slides[slideIndex].url})`,
-    width: `${parentWidth}px`,
-  });
-  const getSlidesContainerStylesWithWidth = () => ({
-    ...slidesContainerStyles,
-    width: parentWidth * slides.length,
-    transform: `translateX(${-(currentIndex * parentWidth)}px)`,
-  });
+function ImageSlider() {
+  const [images, setImages] = useState([]);
 
   useEffect(() => {
-    if (timerRef.current) {
-      clearTimeout(timerRef.current);
-    }
-    timerRef.current = setTimeout(() => {
-      goToNext();
-    }, 3500);
-
-    return () => clearTimeout(timerRef.current);
-  }, [goToNext]);
+    // Fetch the JSON data
+    fetch("/index.json")
+      .then((response) => response.json())
+      .then((data) => setImages(data))
+      .catch((error) => console.error("Error fetching images:", error));
+  }, []);
 
   return (
-    <div style={sliderStyles}>
-      <div>
-        <div onClick={goToPrevious} style={leftArrowStyles}>
-          {leftArrow}
-        </div>
-        <div onClick={goToNext} style={rightArrowStyles}>
-          {rightArrow}
-        </div>
-      </div>
-      <div style={slidesContainerOverflowStyles}>
-        <div style={getSlidesContainerStylesWithWidth()}>
-          {slides.map((_, slideIndex) => (
-            <div
-              key={slideIndex}
-              style={getSlideStylesWithBackground(slideIndex)}
-            ></div>
-          ))}
-        </div>
-      </div>
-      <div style={dotsContainerStyles}>
-        {slides.map((slide, slideIndex) => (
-          <div
-            style={dotStyle}
-            key={slideIndex}
-            onClick={() => goToSlide(slideIndex)}
-          >
-            ●
-          </div>
-        ))}
-      </div>
-    </div>
+    <Carousel className="carousel-container">
+      {images.map((image) => (
+        <Carousel.Item key={image.id} interval={3000}>
+          <img
+            className="d-block mw-100"
+            src={image.image}
+            alt={`Slide ${image.id}`}
+          />
+        </Carousel.Item>
+      ))}
+    </Carousel>
   );
-};
+}
 
 export default ImageSlider;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,38 +1,12 @@
+import React, { useState, useEffect } from "react";
 import ImageSlider from "../components/ImageSlider";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 const App = () => {
-  const slides = [
-    {
-      url: "https://live.staticflickr.com/65535/53116731121_36906cfa41_b.jpg",
-      title: "beach",
-    },
-    {
-      url: "https://live.staticflickr.com/65535/53116133937_f0d4057bbf_b.jpg",
-      title: "boat",
-    },
-    {
-      url: "https://live.staticflickr.com/65535/53116134097_5e38709e38_b.jpg",
-      title: "forest",
-    },
-    {
-      url: "https://live.staticflickr.com/65535/53116932484_0328a736b5_b.jpg",
-      title: "city",
-    },
-    {
-      url: "https://live.staticflickr.com/65535/53116133957_1818ee2f3d_b.jpg",
-      title: "italy",
-    },
-  ];
-  const containerStyles = {
-    width: "64rem",
-    height: "38rem",
-    margin: "0 auto",
-  };
   return (
-    <div>
-      <div style={containerStyles}>
-        <ImageSlider slides={slides} parentWidth={1024} />
-      </div>
-    </div>
+    <>
+      <ImageSlider />
+    </>
   );
 };
 
