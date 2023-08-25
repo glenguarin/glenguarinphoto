a9be13a511e9f8fa1497443210d035c9b4c1bd09
Glen Guarin <glen.guarin@gmail.com>
2023-08-25 16:19:35 +1200

    Added column on contact page

---
+++ src/components/ContacttImageSlider.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+import Carousel from "react-bootstrap/Carousel";
+
+function ContactImageSlider() {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    // Fetch the JSON data
+    fetch("/index.json")
+      .then((response) => response.json())
+      .then((data) => setImages(data))
+      .catch((error) => console.error("Error fetching images:", error));
+  }, []);
+
+  return (
+    <Carousel fade indicators={false} controls={false}>
+      {images.map((image) => (
+        <Carousel.Item key={image.id} interval={3000}>
+          <img
+            className="d-block w-100"
+            src={image.image}
+            alt={`Slide ${image.id}`}
+          />
+        </Carousel.Item>
+      ))}
+    </Carousel>
+  );
+}
+
+export default ContactImageSlider;
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,47 +1,63 @@
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-import { Container } from "react-bootstrap";
+import { Container, Col, Row } from "react-bootstrap";
+import ContactImageSlider from "../components/ContacttImageSlider";
 
 function Contact() {
   return (
     <>
-      <p className="text-center mt-2 pt-5 fs-2 mb-0">Contact me</p>
-      <h1 className="text-center mb-5">
-        Got big plans for an awesome event? Shoot me a message and let's chat!
-      </h1>
+      <p className="text-center mt-2 pt-5 fs-2 mb-5">Contact</p>
       <Container>
-        <Form>
-          <Form.Group className="mb-3" controlId="contactForm.Name">
-            <Form.Label>Name</Form.Label>
-            <Form.Control type="email" placeholder="John Doe" />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="JohnDoe@example.com" />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="contactForm.Name">
-            <Form.Label>Phone Number</Form.Label>
-            <Form.Control type="email" placeholder="+64 21 123 4567" />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="contactForm.Name">
-            <Form.Label>Service your inquiring for</Form.Label>
-            <Form.Control
-              type="email"
-              placeholder="Ex. Wedding Photography, Family Portrait etc."
-            />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="contactForm.Name">
-            <Form.Label>Location to shoot</Form.Label>
-            <Form.Control type="email" placeholder="Auckland" />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-            <Form.Label>Message</Form.Label>
-            <Form.Control as="textarea" rows={3} />
-          </Form.Group>
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
-        </Form>
+        <Row>
+          <Col>
+            <h1 className="text-center mb-5">...Let's Talk</h1>
+            <Form>
+              <Form.Group className="mb-3" controlId="contactForm.Name">
+                <Form.Label>Name</Form.Label>
+                <Form.Control type="email" placeholder="John Doe" />
+              </Form.Group>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlInput2"
+              >
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="JohnDoe@example.com" />
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="contactForm.Name">
+                <Form.Label>Service your inquiring for</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Ex. Wedding Photography, Family Portrait etc."
+                />
+              </Form.Group>
+
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlTextarea1"
+              >
+                <Form.Label>Message</Form.Label>
+                <Form.Control as="textarea" rows={3} />
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                Send Message
+              </Button>
+            </Form>
+          </Col>
+          <Col>
+            <ContactImageSlider />
+            <Row className="justify-content-end">
+              <Col d-flex justify-content-between>
+                <p>E-mail Address</p>
+                <p>glenguarinphoto@gmail.com</p>
+              </Col>
+              <Col>
+                <p>Phone Number</p>
+                <p>021 172 0204</p>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
       </Container>
     </>
   );
